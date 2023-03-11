aedb15af3a47233794f01644c2de738ff6ef8261
meethariya <meetchampian@gmail.com>
2023-03-11 16:19:49 +0530

    unused component

--- market-client/src/app/modules/manager/manager.module.ts
+++ market-client/src/app/modules/manager/manager.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { ManagerHomeComponent } from './components/manager-home/manager-home.component';
-import { ProductComponent } from './components/product/product.component';
 import { ManagerRoutingModule } from './manager-routing.module';
 import { InventoryItemComponent } from './components/inventory-item/inventory-item.component';
 import { AddInventoryComponent } from './components/add-inventory/add-inventory.component';
@@ -18,7 +17,6 @@ import { NgxPaginationModule } from 'ngx-pagination';
 @NgModule({
   declarations: [
     ManagerHomeComponent,
-    ProductComponent,
     InventoryItemComponent,
     AddInventoryComponent,
     AddProductComponent,

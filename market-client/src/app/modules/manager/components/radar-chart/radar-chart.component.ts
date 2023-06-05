5de02d760bc62bbef45dad051191cfe6c51dcc8e
meethariya <meetchampian@gmail.com>
2023-06-05 17:48:45 +0530

    bug fixes

--- market-client/src/app/modules/manager/components/radar-chart/radar-chart.component.ts
+++ market-client/src/app/modules/manager/components/radar-chart/radar-chart.component.ts
@@ -43,7 +43,7 @@ export class RadarChartComponent implements OnInit {
           datasets: [
             {
               data: this.productCount,
-              label: 'Rating in Stars',
+              label: 'Products',
             },
           ],
         };
